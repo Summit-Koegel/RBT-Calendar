1cf87412f725d92729a01c1ec953172b47ace353
Summit Koegel <98934262+Summit-Koegel@users.noreply.github.com>
2022-10-20 01:23:10 -0500

    Update FrontendDeveloperTests.java

--- FrontendDeveloperTests.java
+++ FrontendDeveloperTests.java
@@ -1,11 +1,3 @@
-// --== CS400 Project 02 File Header ==--
-// Name: Aaryush Gupta
-// CSL Username: aaryush
-// Email: agupta276@wisc.edu
-// Lecture #: 004
-// Notes to Grader: I was unable to find a way to mimic user input in JUnit without using
-// libraries I did not understand (and therefore thought dishonest to use).
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
